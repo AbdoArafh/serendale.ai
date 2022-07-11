3d427e37f3ca4b1482fe64fc06309e80974ecc36
Abdurrahman <abdoarafh54@gmail.com>
2022-07-11 08:55:44 +0200

    added: deplyment to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://abdoarafh.github.io/",
   vite: {
     ssr: {
       external: ["svgo"],
