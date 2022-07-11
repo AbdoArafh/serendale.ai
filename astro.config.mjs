77c836c4a435e3de49928bd03e47eb15a8d21c3a
Abdurrahman <abdoarafh54@gmail.com>
2022-07-11 10:43:48 +0200

    fix: trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://abdoarafh.github.io/",
-  base: "/",
+  base: "/serendale.ai",
   vite: {
     ssr: {
       external: ["svgo"],
